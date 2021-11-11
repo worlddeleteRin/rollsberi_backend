ddd126c6192f68c14a19c7860cb95eb3cd70645c
worlddeleteRin <worlddelete0@gmail.com>
2021-11-12 00:44:01 +0300

    :sparkles: working on bonus system

--- main_app/apps/bonuses/bonuses.py
+++ main_app/apps/bonuses/bonuses.py
@@ -1,5 +1,25 @@
 from database.main_db import db_provider
 
+from .bonuses_exceptions import BonusesLevelNotExist
+from .models import BonusesLevel
+
+
 def get_bonuses_levels():
-    pass
+    bonuses_levels_dict = db_provider.bonuses_levels_db.find({})
+    bonuses_levels = [BonusesLevel(**b).dict() for b in bonuses_levels_dict]
+    return bonuses_levels
+
+def get_bonuses_level_by_id(
+    bonuses_level_id: int,
+    silent: bool = False
+):
+    bonuses_level = db_provider.bonuses_levels_db.find_one(
+        {"_id": bonuses_level_id}
+    )
+    if not bonuses_level:
+        if not silent:
+            raise BonusesLevelNotExist
+        return None
+    bonuses_level = BonusesLevel(**bonuses_level)
+    return bonuses_level 
 
