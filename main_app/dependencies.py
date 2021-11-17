50215717627218ebf384bee70a9250c5cc4b04b2
worlddeleteRin <worlddelete0@yandex.ru>
2021-11-17 21:59:24 +0300

    :sparkles:

--- main_app/dependencies.py
+++ main_app/dependencies.py
@@ -4,14 +4,19 @@ from fastapi import HTTPException
 
 from database.main_db import db_provider
 
+import sys
+
 def get_api_app_client(
-    api_key_header:str = Security(APIKeyHeader(name="app_token"))
+    api_key_header:str = Security(APIKeyHeader(name="app_token", auto_error=False))
+    # api_key_header: str = "another_access_token_here"
 ):
+    print('api key query is', api_key_header)
+    sys.stdout.flush()
     app_client_dict = db_provider.app_clients_db.find_one(
         {"access_token": api_key_header}
     )
-    print('api key query is', api_key_header)
     print('client is', app_client_dict)
+    sys.stdout.flush()
 
     if not app_client_dict:
         raise HTTPException(
